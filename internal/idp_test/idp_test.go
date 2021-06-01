66aada6b594b76409b3b6f53c72056febc7d6fac
Quint <me@di-wu.be>
2021-06-01 15:03:46 +0200

    Simplify AzureAD tests.

--- internal/idp_test/idp_test.go
+++ internal/idp_test/idp_test.go
@@ -5,6 +5,7 @@ import (
 	"embed"
 	"encoding/json"
 	"fmt"
+	"github.com/elimity-com/scim"
 	"io"
 	"io/fs"
 	"net/http/httptest"
@@ -20,6 +21,13 @@ var testdata embed.FS
 func TestIdP(t *testing.T) {
 	idPs, _ := testdata.ReadDir("testdata")
 	for _, idP := range idPs {
+		var newServer func() scim.Server
+		switch idP.Name() {
+		case "okta":
+			newServer = newOktaTestServer
+		case "azuread":
+			newServer = newAzureADTestServer
+		}
 		t.Run(idP.Name(), func(t *testing.T) {
 			idpPath := fmt.Sprintf("testdata/%s", idP.Name())
 			if err := fs.WalkDir(testdata, idpPath, func(path string, d fs.DirEntry, err error) error {
@@ -39,7 +47,7 @@ func TestIdP(t *testing.T) {
 				}
 				fileName, _ := filepath.Rel(idpPath, path)
 				t.Run(strings.TrimSuffix(fileName, ".json"), func(t *testing.T) {
-					if err := testRequest(test); err != nil {
+					if err := testRequest(test, newServer); err != nil {
 						t.Error(err)
 					}
 				})
@@ -51,18 +59,18 @@ func TestIdP(t *testing.T) {
 	}
 }
 
-func testRequest(t testCase) error {
+func testRequest(t testCase, newServer func() scim.Server) error {
 	rr := httptest.NewRecorder()
 	var br io.Reader
 	if len(t.Request) != 0 {
 		br = bytes.NewReader(t.Request)
 	}
-	newOktaTestServer().ServeHTTP(
+	newServer().ServeHTTP(
 		rr,
 		httptest.NewRequest(t.Method, t.Path, br),
 	)
 	if code := rr.Code; code != t.StatusCode {
-		return fmt.Errorf("expected %d, got %d", t.StatusCode, code)
+		return fmt.Errorf("expected %d, got %d\n", t.StatusCode, code)
 	}
 	if len(t.Response) != 0 {
 		var response map[string]interface{}
